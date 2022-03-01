38a8c6b7dc0e4ca486ec8b71f80ec9a001008cd0
AM0726Github <Margaryanalik7@gmail.com>
2022-02-28 20:51:42 -0800

    Fixing Bugs and Updating functions to seperate

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const mysql = require('mysql2');
 const db = require('./config/connection');
-const questions = require('./lib/questions');
+const promptMenu = require('./lib/questions');
 
 const PORT = process.env.PORT || 3003;
 const app = express();
 
-// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-// Start server after DB connection
+// Start Promt with function
 db.connect(err => {
     if (err) throw err;
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-        // Run questions from utils/questions.js
-        questions();
+        console.log(`Server now listening port ${PORT}`);
+        promptMenu();
     });
 });
